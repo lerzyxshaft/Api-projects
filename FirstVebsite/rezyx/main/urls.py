7d5c23f9561c22ed1c33c6fd58971112765c742c
Maksym Kozlovskyi <zxcshaft@gmail.com>
2024-12-04 00:38:43 +0100

    First Website proj

--- FirstVebsite/rezyx/main/urls.py
+++ FirstVebsite/rezyx/main/urls.py
@@ -5,6 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about)
+    path('', views.index, name = 'home'),
+    path('about', views.about, name = "about")
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
