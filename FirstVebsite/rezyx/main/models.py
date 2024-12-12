07d2cf9d775f1a852e3ec799bc8af53e36b62334
Maksym Kozlovskyi <zxcshaft@gmail.com>
2024-12-12 22:47:19 +0100

    First Website proj

--- FirstVebsite/rezyx/main/models.py
+++ FirstVebsite/rezyx/main/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
 class Articles(models.Model):
-    title = models.CharField('Name', max_length=50, default='name')
-    announcements = models.CharField('announcement', max_length=250, default='name')
-    full_text = models.TextField('Article')
-    date = models.DateTimeField('Publication date')
+    title = models.CharField(max_length=100)
+    anons = models.TextField()
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)  # Field name
 
     def __str__(self):
         return self.title
 
+
     class Meta:
         verbose_name = "News"
         verbose_name_plural = "News"
+
+    class NewsArticle(models.Model):
+        title = models.CharField(max_length=255)
+        anons = models.TextField()
--- FirstVebsite/rezyx/news/models.py
+++ FirstVebsite/rezyx/news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Articles(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+
