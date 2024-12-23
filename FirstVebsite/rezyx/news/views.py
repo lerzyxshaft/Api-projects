7c9bf6039f6bfbd894f2dd98dfe574d61f49ec4a
Maksym Kozlovskyi <zxcshaft@gmail.com>
2024-12-23 22:39:36 +0100

    First Website proj 22Dec

--- FirstVebsite/rezyx/news/views.py
+++ FirstVebsite/rezyx/news/views.py
@@ -4,3 +4,4 @@ from .models import Articles
 def news_home(request):
     news = Articles.objects.order_by('-date')  # Corrected line
     return render(request, 'news/news_home.html', {'news': news})
+
